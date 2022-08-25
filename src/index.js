bae7c76475858a85ce5eb046557bc8bc53c232b2
arvind <developer.arvind003@gmail.com>
2022-08-25 14:13:07 +0530

    First component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import React from 'react'
+ import ReactDOM  from 'react-dom'
+function Greeting(){
+return <h2>this is first component</h2>
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'))
