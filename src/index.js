03bced389c979dff4f598127fb7aa6f2c215350a
arvind <developer.arvind003@gmail.com>
2022-08-30 01:10:35 +0530

    JSX

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ function Greeting(){
 return <h2>this is first component</h2>
 }
 
+// we use the above format bcoz easy tio maintain the code and return
+
+// const Greeting =() =>{
+// 	return React.createElement(
+	
+// 	'p',{},React.createElement('h1', {}, 'Hello world'))
+// }
+
+
 ReactDOM.render(<Greeting/>, document.getElementById('root'))
